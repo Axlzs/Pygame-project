f832745692cfee73c105da7a13980236bda1543c
Axlzs <90036459+Axlzs@users.noreply.github.com>
2024-12-06 19:42:55 +0200

    mom im going pro
    the change  has been started

--- static_variables.py
+++ static_variables.py
@@ -47,8 +47,22 @@ class Static_variables:
     XP_SCALE = 1.25
     STARTING_XP = 4
     PLAYER_DATA = {
-        1: {'image':'images/players/player1.png','sprite':48,'hitbox_width':16,'hitbox_height':28,'class':1,'health':50,'damage':8},
-        2: {'image':'images/players/player2.png','sprite':64,'hitbox_width':10,'hitbox_height':20,'class':2,'health':100,'damage':20,'range':20,'heal_factor':1},
+        1: {'image':'images/players/player_ranged.png','sprite':64,
+            'walk_up':      {'row':5,'frames':9},
+            'walk_left':    {'row':6,'frames':9},
+            'walk_down':    {'row':7,'frames':9},
+            'walk_right':   {'row':8,'frames':9},
+
+            'attack_up':    {'row':1,'frames':8},
+            'attack_left':  {'row':2,'frames':8},
+            'attack_down':  {'row':3,'frames':8},
+            'attack_right': {'row':4,'frames':8},
+
+            'death':        {'row':9,'frames':6},
+            'hitbox_width':16,'hitbox_height':28,'class':1,'health':50,'damage':8},
+        2: {'image':'images/players/player_knihgt.png','sprite':64,
+            
+            'hitbox_width':16,'hitbox_height':28,'class':2,'health':100,'damage':20,'range':20,'heal_factor':1},
         3: ''
     }
     ARROW_LIFESPAN = 5000
